71a83c676b5a68cc3e390ebd3964efb70c371675
Elena Kanash <hird250@gmail.com>
2022-04-22 21:26:37 +0300

    feat: add burger menu to main and pets pages

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,2 +1,23 @@
-let a = 42;
-console.log(a);
+const wrapper = document.querySelector('.header__wrapper'),
+      overlay = document.querySelector('.nav-overley'),
+      burgerBtn = document.querySelector('.burger'),
+      menu = document.querySelector('.nav'),
+      menuLinks = [...document.querySelectorAll('.nav__link')];
+
+
+burgerBtn.addEventListener('click', openMenu);
+menuLinks.forEach(el =>
+  el.addEventListener('click', openMenu));
+
+function  openMenu() {
+  menu.classList.toggle('active');
+  overlay.classList.toggle('active');
+  wrapper.classList.toggle('active');
+  burgerBtn.classList.toggle('active');
+}
+
+
+
+
+
+
