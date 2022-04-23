50acce8477ca2d2ba4b4523bb3f35cb89f8b65b7
Elena Kanash <hird250@gmail.com>
2022-04-23 13:06:24 +0300

    fix: fix errors in burger

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -5,18 +5,25 @@ const wrapper = document.querySelector('.header__wrapper'),
       menuLinks = [...document.querySelectorAll('.nav__link')];
       console.log(menuLinks)
 
+//menuLinks[2].classList.toggle('disabled');
+//menuLinks[3].classList.toggle('disabled');
 
-burgerBtn.addEventListener('click', toggleMenu);
+
+burgerBtn.addEventListener('click', toggleMenu); 
+
 menuLinks.forEach(el =>
   el.addEventListener('click', toggleMenu));
   overlay.addEventListener('click', toggleMenu);
+
+  menuLinks.forEach(el =>
+    el.addEventListener('click', () => {
+      document.body.style.overflow = "visible"
+    }));
 
 function  toggleMenu() {
   menu.classList.toggle('active');
   overlay.classList.toggle('active');
   wrapper.classList.toggle('active');
   burgerBtn.classList.toggle('active');
-  menuLinks[2].classList.toggle('disabled');
-  menuLinks[3].classList.toggle('disabled');
   document.body.style.overflow = "hidden";//запретить скролл при открытии меню
 }
