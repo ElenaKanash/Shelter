31eb8a1a420d89ce1e7a2be43de7a0e7b0f5c49e
Elena Kanash <hird250@gmail.com>
2022-04-23 12:09:37 +0300

    feat: finalize burger menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -5,15 +5,20 @@ const wrapper = document.querySelector('.header__wrapper'),
       menuLinks = [...document.querySelectorAll('.nav__link')];
 
 
-burgerBtn.addEventListener('click', openMenu);
+burgerBtn.addEventListener('click', toggleMenu);
 menuLinks.forEach(el =>
-  el.addEventListener('click', openMenu));
+  el.addEventListener('click', toggleMenu));
 
-function  openMenu() {
+overlay.addEventListener('click', toggleMenu);
+
+function  toggleMenu() {
   menu.classList.toggle('active');
   overlay.classList.toggle('active');
   wrapper.classList.toggle('active');
   burgerBtn.classList.toggle('active');
+  menuLinks[2].classList.toggle('disabled');
+  menuLinks[3].classList.toggle('disabled');
+  document.body.style.overflow = "hidden"; //запретить скролл при открытии меню
 }
 
 
