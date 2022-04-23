ccf1c3d20e595caf843a4c4f68e41481deb52f67
Elena Kanash <hird250@gmail.com>
2022-04-23 13:06:24 +0300

    fix: fix errors in burger

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -2,22 +2,32 @@ const wrapper = document.querySelector('.header__wrapper'),
       overlay = document.querySelector('.nav-overley'),
       burgerBtn = document.querySelector('.burger'),
       menu = document.querySelector('.nav'),
-      menuLinks = [...document.querySelectorAll('.nav__link')];
+      menuLinks = [...document.querySelectorAll('.nav__link')],
+      logoMain = document.querySelector('.logo__link_main');
 
 
-burgerBtn.addEventListener('click', toggleMenu);
+logoMain.classList.add('disabled'); //блок перехода по ссылке по клику на лого на главной странице
+//menuLinks[2].classList.toggle('disabled');
+//menuLinks[3].classList.toggle('disabled');
+document.body.style.overflow = "visible";
+
+burgerBtn.addEventListener('click', toggleMenu);//открываем иеню по клику на кнопку
+
 menuLinks.forEach(el =>
-  el.addEventListener('click', toggleMenu));
+  el.addEventListener('click', toggleMenu));//закрываем меню по клику на ссылку
 
-overlay.addEventListener('click', toggleMenu);
+  menuLinks.forEach(el =>
+    el.addEventListener('click', () => {
+      document.body.style.overflow = "visible"
+    })); //разрешаем скролл при клике на ссылку
+
+overlay.addEventListener('click', toggleMenu); //открываем блок с затемнением
 
 function  toggleMenu() {
   menu.classList.toggle('active');
   overlay.classList.toggle('active');
   wrapper.classList.toggle('active');
   burgerBtn.classList.toggle('active');
-  menuLinks[2].classList.toggle('disabled');
-  menuLinks[3].classList.toggle('disabled');
   document.body.style.overflow = "hidden"; //запретить скролл при открытии меню
 }
 
