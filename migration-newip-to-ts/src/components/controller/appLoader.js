93b7dd88c617485a9d86454b03d45367c5c55bae
Elena Kanash <hird250@gmail.com>
2022-07-04 22:36:06 +0300

    feat: add API key to appLoader,js

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '4e3bd25c3927473687295d0495c2aa48', // получите свой ключ https://newsapi.org/
         });
     }
 }
